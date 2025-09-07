e671501af8a4db00025ecae6035163d7034a8a09
Jakia Rahman <jakiarahman0701@gmail.com>
2025-09-07 17:02:21 +0600

    New Tasks

---
+++ All Tasks/Javascript Tutorial Practice/task12.js
@@ -0,0 +1,14 @@
+//direclty select id wise content and change html 
+
+// -- document.getElementById("heading1").innerHTML = "Hello";
+
+//select the id wise content and save in a variable then change element
+
+let save = document.getElementById("heading1");
+save.innerHTML = "Jakia";
+
+document.getElementsByTagName("h1")[0].innerHTML = "HI" ;
+
+document.getElementsByClassName("heading3")[2].innerHTML = "HELLO" ;
+
+document.getElementsByTagName("p")[3].innerHTML = "WORLD";
